1238f26559c7b2c52791a5cf565fac0113de5714
guilhermesilvafonseca <guilherme.fonseca@bandtec.com>
2021-09-07 18:16:57 -0300

    indexx

---
+++ Site institucional/JavaScript/script.js
@@ -0,0 +1,17 @@
+// const imgs = document.getElementById("img");
+// const img = document.querySelectorAll("#img img");
+
+// let idx = 0;
+
+// function carrossel(){
+//     idx++;
+
+//     if(idx > img.length - 1){
+//         idx = 0;
+//     }
+
+//     imgs.style.transform = `translateX(${-idx * 100}%)`;
+
+// }
+
+// setInterval(carrossel, 1800);
